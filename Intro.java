a5b676b6a13b5ac645b33d1a195e1ba004fd6374
Justin <XxChooChooPiexX@gmail.com>
2015-05-08 21:47:59 -0400

    first commit

---
+++ Intro.java
@@ -0,0 +1,31 @@
+
+public class Intro {
+	private String message;
+	
+	public Intro(){
+		this.message = "hello";
+	}
+	
+	public Intro(String message){
+		this.message = message;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String message){
+		this.message = message;
+	}
+	
+	public static void main(String args[]){
+		Intro x = new Intro();
+		System.out.println(x.getMessage());
+		Intro y = new Intro("what's up");
+		System.out.println(y.getMessage());
+		x.setMessage("Lets have fun");
+		System.out.println(x.getMessage());
+
+	}
+
+}
