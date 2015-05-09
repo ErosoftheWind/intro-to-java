f5ef3d5e18d0ee39f3fad28ff973493ee6d27139
Justin <XxChooChooPiexX@gmail.com>
2015-05-08 22:08:58 -0400

    created test file, add more stuff  to intro.java

--- Intro.java
+++ Intro.java
@@ -1,35 +1,45 @@
 
 public class Intro {
 	private String message;
+	private String name;
 	
 	public Intro(){
 		this.message = "hello";
+		this.name = "justin";
 	}
 	
-	public Intro(String message){
+	public Intro(String message, String name){
 		this.message = message;
+		this.name = name;
 	}
 	
 	public String getMessage(){
-		return message;
+		return this.message;
+	}
+	
+	public String getName(){
+		return this.name;
 	}
 	
 	public void setMessage(String message){
 		this.message = message;
 	}
 	
+	public void setName(String name){
+		this.name = name;
+	}
+	
 	public void print(){
-		
+		System.out.println(this.message + " " + this.name);
 	}
 	
 	public static void main(String args[]){
 		Intro x = new Intro();
-		System.out.println(x.getMessage());
-		Intro y = new Intro("what's up");
-		System.out.println(y.getMessage());
+		x.print();
+		Intro y = new Intro("what's up", "derek");
+		y.print();
 		x.setMessage("Lets have fun");
-		System.out.println(x.getMessage());
-		
+		x.print();
 	}
 
 }
---
+++ IntroTest.java
@@ -0,0 +1,7 @@
+
+public class IntroTest {
+	public static void main(String args[]){
+		Intro test = new Intro();
+		test.print();
+	}
+}
